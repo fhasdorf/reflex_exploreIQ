1d0e43112e03ced87f04e1389d828fff2b945ef5
Frank Hasdorf <frank@hasdorf.de>
2026-05-09 14:16:00 +0200

    delete folder mineral_app

--- mineral_app/faq/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Frank Hasdorf
-# @Date:   06-05-2026 18:16:33
-# @Last Modified by:   Frank Hasdorf
-# @Last Modified time: 06-05-2026 18:17:01
-
-
-
--- mineral_app/route_snippet.py
+++
@@ -1,9 +0,0 @@
-# In mineral_app.py — diese zwei Zeilen am Ende ergänzen:
-
-from mineral_app.faq.prospektivitaet import faq_prospektivitaet_page
-
-app.add_page(
-    faq_prospektivitaet_page,
-    route="/faq/prospektivitaet",
-    title="EMI ExploreIQ · Hilfe Prospektivität",
-)
